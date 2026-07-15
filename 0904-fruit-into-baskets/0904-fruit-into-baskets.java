1c69753d0ac2a57db6f7e2f93213003e56bb2db7
Bhavya-0006 <jainbhavya06@gmail.com>
2026-07-15 10:50:32 +0530

    Time: 6 ms (99.28%), Space: 73.3 MB (5.65%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,33 +1,25 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        int i=0, j=0, n=fruits.length;
-        Map<Integer,Integer> mp = new HashMap<>();
-        int mx = 0;
-        while (j<n){
-            mp.put(fruits[j] , mp.getOrDefault(fruits[j],0) + 1);
-            if(mp.size() < 2){
-                mx = Math.max(mx , (j-i+1));
-                j++;
-            }
-            else if(mp.size() == 2){
-                mx = Math.max(mx , (j-i+1));
-                j++;
-            }
-            else if(mp.size()>2){
-                while(mp.size()>2){
-                    mp.put(fruits[i], mp.get(fruits[i])-1);
+        int maxlen = 0;
+        int lastFruit = -1;
+        int secondLastFruit = -1;
+        int currCount = 0;
+        int lastFruitStreak = 0;
+        for (int f : fruits){
+            if(f == lastFruit || f == secondLastFruit) currCount++;
+            else {
+                currCount = lastFruitStreak + 1;
 
-                    if(mp.get(fruits[i]) == 0){
-                        mp.remove(fruits[i]);
-                    }
-                    i++;
-                }
-                if(mp.size() == 2){
-                    mx = Math.max(mx , (j-i+1));
-                }
-                j++;
             }
+            if (f == lastFruit) lastFruitStreak++;
+            else{
+                lastFruitStreak = 1;
+                secondLastFruit = lastFruit;
+                lastFruit = f;
+            }
+            maxlen = Math.max(maxlen , currCount);
         }
-        return mx;
+        return maxlen;
+
     }
 }

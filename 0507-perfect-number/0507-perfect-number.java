f1ca3e66ffe4499bddc09de37461457de0824599
Bhavya-0006 <jainbhavya06@gmail.com>
2026-07-24 09:39:01 +0530

    Time: 2078 ms (59.74%), Space: 42.1 MB (62.83%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int n = num ; 
+        int sum = 0;
+        for(int i=1; i<n-1; i++){
+            if(n%i==0) sum += i;
+        }
+        return sum == num;
+    }
+}
